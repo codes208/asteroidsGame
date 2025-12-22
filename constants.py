3e3eddccf37a4c12e3f9a4c4a992ef4793e97c0b
Cody Gerrard <cody.gerrard@wsu.edu>
2025-12-21 21:33:56 -0800

    Added a few classes and made the triangle

--- constants.py
+++ constants.py
@@ -1,3 +1,8 @@
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
+# Radius of the player's ship
+PLAYER_RADIUS = 20
+
+# Width fo the lines that draw the player's ship
+LINE_WIDTH = 2
--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    fps_clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     while 1:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("red")
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = fps_clock.tick(60) / 1000
 
     print("Starting Asteroids with pygame version: 2.6.1")
     print(f"Screen width: {SCREEN_WIDTH}")
